8f53bf636600c2184932409a03a2201003f9e881
mutaihillary <mutaihillary@yahoo.com>
2016-11-10 22:57:43 +0300

    key_press code

--- key_press/key_press.py
+++ key_press/key_press.py
@@ -1,10 +1,10 @@
 def presses(str):
-    # Your Code Here!
+# Your Code Here!
 
 
 
-print presses("V8") # 7
-print presses("LOL") # 9
-print presses("How R  2day") #23
-print presses("i 8 2 Many mandazi 4 brekky")
+    print presses("V8") # 7
+    print presses("LOL") # 9
+    print presses("How R  2day") #23
+    print presses("i 8 2 Many mandazi 4 brekky")
 
